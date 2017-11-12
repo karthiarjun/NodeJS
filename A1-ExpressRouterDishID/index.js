cfe680b3689c19e8cb38a833ef0f21c5b2a306b3
karthiarjun <arjunkarthik1991@gmail.com>
2017-11-12 23:50:30 +0530

    Assignment:1-
    To assign dishesID in express router

---
+++ A1-ExpressRouterDishID/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const http = require('http')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const hostname='localhost';
+const port = 3000;
+
+const app = express();
+const dishRouter = require('./router/dishRouter');
+app.use(morgan('dev'));
+app.use(bodyParser.json()); //adding bodyparser to make json response
+app.use('/dishes',dishRouter); //Mount dishRouter
+
+app.use(express.static(__dirname+'/public')); //access file from folder
+
+
+ app.use((req,res,next) => {
+	console.log(req.headers);
+	res.statusCode = 200;
+	res.setHeader('Content-type','text/html');
+	res.end('<html> <body> <h1> Express server </h1> </body></html>')
+}); 
+
+const server = http.createServer(app);
+server.listen(port,hostname,() => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+})
+
+//by default http://localhost:3000/ it refers to index.html
+
+//http://localhost:3000/abt.html
