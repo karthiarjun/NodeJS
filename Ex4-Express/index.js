86ee173629f3f7447453f5b1cddfe345b9006549
karthiarjun <arjunkarthik1991@gmail.com>
2017-11-12 19:41:27 +0530

    express morgan

--- Ex4-Express/index.js
+++ Ex4-Express/index.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const http = require('http')
+const morgan = require('morgan')
 const hostname='localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
 
-app.use((req,res,next) => {
+app.use(express.static(__dirname+'/public')); //access file from folder
+
+
+ app.use((req,res,next) => {
 	console.log(req.headers);
 	res.statusCode = 200;
 	res.setHeader('Content-type','text/html');
 	res.end('<html> <body> <h1> Express server </h1> </body></html>')
-});
+}); 
 
 const server = http.createServer(app);
 server.listen(port,hostname,() => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 })
+
+//by default http://localhost:3000/ it refers to index.html
+
+//http://localhost:3000/abt.html
